ed43e6ff2961c16f46fac4e004916e80518b4e84
Sergey <tarkus.gsa@gmail.com>
2020-05-18 19:11:39 +0300

    Update adatis-v14.conf and update-device.sh

--- update-device.sh
+++
@@ -1,6 +0,0 @@
-A20_IP="192.168.1.103"
-
-sshpass -p 'adatis' scp ./output/debs/linux-dtb-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root
-sshpass -p 'adatis' scp ./output/debs/linux-image-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root
-sshpass -p 'adatis' scp ./output/debs/linux-u-boot-current-adatis-v14_20.05.0-trunk_armhf.deb root@$A20_IP:/root
-sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync && reboot'
