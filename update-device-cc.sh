1767fd7a56c1a2fc64755e5877fab429cbcb4e12
TarkusGSA <tarkus.gsa@gmail.com>
2021-04-30 14:14:56 +0300

    A20 v1.7 added

--- update-device-cc.sh
+++ update-device-cc.sh
@@ -1,9 +1,9 @@
-A20_IP="192.168.1.201"
+A20_IP="192.168.1.106"
 
 sshpass -p 'adatis' scp ./output/debs/linux-dtb-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root
 sshpass -p 'adatis' scp ./output/debs/linux-image-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root
 sshpass -p 'adatis' scp ./output/debs/linux-u-boot-current-adatis-cc_20.05.0-trunk_armhf.deb root@$A20_IP:/root
-sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync'
-#sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync && reboot'
+#sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync'
+sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync && reboot'
 
 #sudo  chmod -R 4777 ./cache/sources/linux-mainline/orange-pi-5.4
