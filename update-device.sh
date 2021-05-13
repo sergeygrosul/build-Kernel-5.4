8d72707b8842278bd89af4d66610bddfcc56ccf5
TarkusGSA <tarkus.gsa@gmail.com>
2021-05-13 13:26:38 +0300

    Moved STMMAC patches to the Userpatches

--- update-device.sh
+++ update-device.sh
@@ -1,9 +1,13 @@
 A20_IP="192.168.1.102"
 
-sshpass -p 'adatis' scp ./output/debs/linux-dtb-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root
-sshpass -p 'adatis' scp ./output/debs/linux-image-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root
-sshpass -p 'adatis' scp ./output/debs/linux-u-boot-current-adatis-v16_20.05.0-trunk_armhf.deb root@$A20_IP:/root
+echo "Copy DTB file..."
+sshpass -p 'adatis' scp ./output/debs/linux-dtb-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root/deb
+echo "Copy the Kernel..."
+sshpass -p 'adatis' scp ./output/debs/linux-image-current-sunxi_20.05.0-trunk_armhf.deb root@$A20_IP:/root/deb
+echo "Copy the u-boot..."
+sshpass -p 'adatis' scp ./output/debs/linux-u-boot-current-adatis-v16_20.05.0-trunk_armhf.deb root@$A20_IP:/root/deb
 #sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync'
-sshpass -p 'adatis' ssh root@$A20_IP 'dpkg -i *.deb && sync && reboot'
+echo "Installing ..."
+sshpass -p 'adatis' ssh root@$A20_IP 'cd /root/deb && dpkg -i *.deb && sync && reboot'
 
 #sudo  chmod -R 4777 ./cache/sources/linux-mainline/orange-pi-5.4
